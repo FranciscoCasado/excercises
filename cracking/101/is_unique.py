b5df0d9b799e59e8b762ff70887015cb0756e6e5
Francisco Casado <fcasado@ug.uchile.cl>
2021-12-23 20:35:12 -0300

    Added ex 101

---
+++ cracking/101/is_unique.py
@@ -0,0 +1,6 @@
+"""
+Implement an algorithm to determine if a string has all unique characters. What if u cannot use additional data structures?
+"""
+
+def is_unique(str) -> bool :
+    return True
---
+++ cracking/101/test_is_unique.py
@@ -0,0 +1,15 @@
+import unittest
+
+from is_unique import is_unique
+
+class TestIsUnique(unittest.TestCase):
+
+    def test_unique_string(self):
+        self.assertTrue(is_unique('hola'))
+
+    
+    def test_non_unique_string(self):
+        self.assertFalse(is_unique('no es unica'))
+
+if __name__ == '__main__':
+    unittest.main()
