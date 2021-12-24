465c7747a8e5e3544931ac0149e9849b199990f9
Francisco Casado <fcasado@ug.uchile.cl>
2021-12-23 22:28:42 -0300

    solved 107 in place

--- cracking/107/rotate_matrix.py
+++ cracking/107/rotate_matrix.py
@@ -8,12 +8,35 @@ class Matrix:
 
     def __init__(self, size):
         self._size = size
-        self.data = [ [0]*size ] * size
+        self.data = []
+
+        for i in range(size):
+            self.data.append([])
+            for j in range(size):
+                self.data[i].append(0)
+
 
     @property
     def size(self):
         return self._size
 
+    def __repr__(self):
+        s = ''
+        for l in self.data:
+            s += str(l) + '\n' 
+        return s
+
 def rotate_matrix(img) -> Matrix:
+    N = img.size
+    stop = int(N/2) + (N%2)
+
+    for i in range(stop):
+        for j in range(stop):
+            temp = img.data[i][j]
+            img.data[i][j] = img.data[j][N-1-i]
+            img.data[j][N-1-i] = img.data[N-1-i][N-1-j]
+            img.data[N-1-i][N-1-j] = img.data[N-1-j][i]
+            img.data[N-1-j][i] = temp
+
     return img
     
--- cracking/107/test_rotate_matrix.py
+++ cracking/107/test_rotate_matrix.py
@@ -6,13 +6,33 @@ from rotate_matrix import Matrix, rotate_matrix
 
 class TestRotateMatrix(unittest.TestCase):
 
-    def test_empty_matrix(self):
+    def test_rotate_empty_matrix(self):
         z = Matrix(4)
         p = Matrix(4)
 
+        rotate_matrix(z)
         for i in range(z.size):
-            rotate_matrix(z)
             self.assertCountEqual(z.data[i], p.data[i])
+    
+    def test_rotate_matrix(self):
+        p = Matrix(4)
+        p.data[1][2] = 56
+        q = Matrix(4)
+        q.data[1][1] = 56
+
+        rotate_matrix(p)
+        for i in range(p.size):
+            self.assertCountEqual(p.data[i], q.data[i])
+        
+    def test_rotate_matrix_odd_size(self):
+        p = Matrix(9)
+        p.data[1][8] = 56
+        q = Matrix(9)
+        q.data[0][1] = 56
+
+        rotate_matrix(p)
+        for i in range(p.size):
+            self.assertCountEqual(p.data[i], q.data[i])
 
 
 if __name__ == '__main__':
