b79412a6356168e6f9580050533bd36b4dd855ef
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-04 23:57:41 -0300

    Added __eq__ method to Tower class

--- cracking/806/hanoi.py
+++ cracking/806/hanoi.py
@@ -3,8 +3,7 @@ from collections import deque
 
 class Tower:
     def __init__(self, n=0):
-        self._list = deque([n-i for i in range(n)])
-        
+        self._list = deque([n - i for i in range(n)])
 
     def pop(self):
         if not self._list:
@@ -13,18 +12,25 @@ class Tower:
 
     def push(self, i):
         if self._list:
-            if i > self._list[-1] :
+            if i > self._list[-1]:
                 raise HanoiDiscError("disc too big for this tower")
         self._list.append(i)
 
     def __repr__(self):
         return str(list(self._list))
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self._list == other._list
+
+        return False
+
+
 class HanoiDiscError(Exception):
     pass
 
-class HanoiTowers:
 
+class HanoiTowers:
     def __init__(self, n):
         self._towers = [Tower(n)]
         self._towers.append(Tower())
@@ -42,10 +48,9 @@ class HanoiTowers:
             dest.push(src.pop())
             return
 
-        self.move_tower(src, aux, dest, n-1)
+        self.move_tower(src, aux, dest, n - 1)
         dest.push(src.pop())
-        self.move_tower(aux, dest, src, n-1)
-
+        self.move_tower(aux, dest, src, n - 1)
 
     def __repr__(self):
         a = "A:" + repr(self._towers[0])
--- cracking/806/test_hanoi.py
+++ cracking/806/test_hanoi.py
@@ -2,12 +2,13 @@ import unittest
 
 from hanoi import Tower, HanoiTowers, HanoiDiscError
 
+
 class TestTower(unittest.TestCase):
     def test_empty_tower(self):
         t = Tower()
         self.assertFalse(t._list)
         self.assertIsNone(t.pop())
-        
+
     def test_push_element_into_empty_tower(self):
         t = Tower()
         t.push(16)
@@ -19,20 +20,39 @@ class TestTower(unittest.TestCase):
         self.assertIsNotNone(t._list)
         self.assertEqual(list(t._list), [5, 4, 3, 2, 1])
         self.assertEqual(t.pop(), 1)
-        
-    
+
+    def test_equality(self):
+        a = Tower(5)
+        self.assertTrue(a.__eq__(Tower(5)))
+        self.assertFalse(a.__eq__(Tower(4)))
+
     def test_push_bigger_disc_and_raise_error(self):
         t = Tower(5)
         self.assertRaises(HanoiDiscError, t.push, 45)
 
     def test_tower_repr(self):
         t = Tower()
-        self.assertEqual(repr(t),"[]")
+        self.assertEqual(repr(t), "[]")
         t = Tower(5)
-        self.assertEqual(repr(t),"[5, 4, 3, 2, 1]")
+        self.assertEqual(repr(t), "[5, 4, 3, 2, 1]")
 
-    
 
+class TestHanoiTowers(unittest.TestCase):
+    def test_create_hanoi_towers(self):
+        h = HanoiTowers(5)
+        self.assertEqual(len(h._towers), 3)
+        self.assertEqual(repr(h._towers[0]), repr(Tower(5)))
+
+    def test_move_tower(self):
+        h = HanoiTowers(1)
+
+        [a, b, c] = h._towers
+
+        h.move_tower(a, b, c, 1)
+
+        self.assertEqual(a, Tower())
+        self.assertEqual(b, Tower(1))
+        self.assertEqual(c, Tower())
 
 
 if __name__ == "__main__":
