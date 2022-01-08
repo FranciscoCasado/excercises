b9381732c2aab1dee06631517dc2649b766f2ec8
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-08 15:12:36 -0300

    aded swap in place

---
+++ cracking/1601/swap_in_place.py
@@ -0,0 +1,7 @@
+
+
+def swap(a, b):
+    a = a - b
+    b = b + a
+    a = b - a
+    return a,b
---
+++ cracking/1601/test_swap_in_place.py
@@ -0,0 +1,12 @@
+import unittest
+
+from swap_in_place import swap
+
+class TestSwapInPlace(unittest.TestCase):
+    def test_swap_numbers(self):
+        a = 5
+        b = 15
+        self.assertEqual(swap(a,b), (15,5))
+
+if __name__ == "__main__":
+    unittest.main()
