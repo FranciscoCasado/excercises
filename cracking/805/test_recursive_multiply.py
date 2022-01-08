b4b6525170366ed12b55f78c0bc7984467641796
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-08 14:57:44 -0300

    optimized recursive multiply

--- cracking/805/test_recursive_multiply.py
+++ cracking/805/test_recursive_multiply.py
@@ -11,6 +11,11 @@ class TestRecursiveMultiply(unittest.TestCase):
     def test_multiply_by_one(self):
         self.assertEqual(multiply(5,1), 5)
         self.assertEqual(multiply(1,3), 3)
+
+
+    def test_multiply_by_two_numbers(self):
+        self.assertEqual(multiply(5,2), 10)
+        self.assertEqual(multiply(2,3), 6)
         
 
 if __name__ == "__main__":
