962cec770f558c288348b6d0b3643c6090303e17
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-08 15:18:07 -0300

    added todo

--- cracking/1601/swap_in_place.py
+++ cracking/1601/swap_in_place.py
@@ -4,4 +4,9 @@ def swap(a, b):
     a = a - b
     b = b + a
     a = b - a
-    return a,b
+    return a, b
+
+#TODO
+def swap_bitwse(a, b):
+    "Use xor (^) operator to swap two data in place"
+    return a, b
