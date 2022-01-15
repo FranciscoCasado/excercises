84772fba87a3ba19929d04efb4a7c73f48a15f08
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-15 10:29:05 -0300

    replaced xor() with ^

--- cracking/ex_1701/add_without_plus.py
+++ cracking/ex_1701/add_without_plus.py
@@ -1,28 +1,25 @@
-from operator import xor
-
 def add(a: int, b: int):
     # Python allocates ints in 4 bytes!
-    s = 0   # sum
-    c = 0   # carry
+    s = 0  # sum
+    c = 0  # carry
     for i in range(32):
         value = bit_sum(bit(a, i), bit(b, i), c)
         s = set_bit(s, value, i)
         c = carry(bit(a, i), bit(b, i), c)
     return s
 
+
 def bit(a: int, index: int) -> bool:
     return (a >> index) & 1
+
 
 def bit_sum(a: int, b: int, c: int) -> bool:
     return a ^ b ^ c
 
+
 def carry(a: int, b: int, c: int) -> bool:
-    return ( 
-        (a & b & c) ^ 
-        (a & b & (not c)) ^ 
-        ( (not a) & b & c) ^ 
-        (a & (not b) & c )
-    )
+    return (a & b & c) ^ (a & b & (not c)) ^ ((not a) & b & c) ^ (a & (not b) & c)
+
 
 def set_bit(a: int, value: int, index: int) -> int:
     if value < 0 or value > 1:
