4206a803ed5e1ca6872a75bd67b8933ecf692cf2
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-15 15:56:33 -0300

    refatored for readability

--- cracking/ex_1708/circus_tower.py
+++ cracking/ex_1708/circus_tower.py
@@ -1,20 +1,24 @@
 class Node:
-    def __init__(self, data=(0,0), height=1,parent=None, children=None):
+    def __init__(self, data=(0, 0), height=1, parent=None, children=None):
         self.data = data
         self.parent = parent
         self.children = []
-        self.tower_height = height
+        self.height = height
 
     def append_child(self, new_child):
-        new_child.tower_height += self.tower_height
+        new_child.height += self.height
         new_child.parent = self
         self.children.append(new_child)
 
     def __le__(self, other):
-        return (self.data[0] <= other.data[0]) and (self.data[1] <= other.data[1]) 
-    
+        return (self.data[0] <= other.data[0]) and (self.data[1] <= other.data[1])
+
+    def __ge__(self, other):
+        return (self.data[0] >= other.data[0]) and (self.data[1] >= other.data[1])
+
     def __repr__(self):
         return str(self.data)
+
 
 class Tree:
     def __init__(self, root):
@@ -25,36 +29,42 @@ class Tree:
         if not self.leaves:
             self.root.append_child(new_child)
             self.leaves.append(new_child)
-            return 
+            return
+        spot = self._find_best_spot_for_new_child(new_child)
+        self._append_new_child(spot, new_child)
 
-        candidates = []
+    def _find_best_spot_for_new_child(self, new_child):
+        best = self.root
         for child in self.leaves:
             current = child
-            print(f"{current}:{new_child}")
             while current.parent:
                 if current <= new_child:
                     break
                 current = current.parent
-            candidates.append(current)
+            best = current if current.height >= best.height else best
 
-        best = candidates[0]
-        for candidate in candidates:
-            best = candidate if candidate.tower_height >= best.tower_height else best
-        
-        best.append_child(new_child)
+        return best
+
+    def _append_new_child(self, spot, new_child):
+        spot.append_child(new_child)
         self.leaves.append(new_child)
-        if best in self.leaves:
-            self.leaves.remove(best)
+        if spot in self.leaves:
+            self.leaves.remove(spot)
+
+    def largest_branch(self):
+        largest = self.root
+        for leaf in self.leaves:
+            largest = leaf if leaf.height >= self.root.height else largest
+        return largest
 
 
-def largest_tower(people: list):
+def largest_tower(people: list) -> list:
     tower_height = 0
     people.sort()
     nodes = [Node(person) for person in people]
     tree = Tree(Node(height=0))
-    
+
     for node in nodes:
         tree.add_child(node)
-        tower_height = max(node.tower_height, tower_height)
-    
-    return tower_height
+
+    return tree.largest_branch()
--- cracking/ex_1708/tests/test_circus_tower.py
+++ cracking/ex_1708/tests/test_circus_tower.py
@@ -6,21 +6,33 @@ from cracking.ex_1708.circus_tower import largest_tower
 class TestCircusTower(unittest.TestCase):
     def test_one_person_tower(self):
         people = [(65, 100)]
-        self.assertEqual(largest_tower(people), 1)
+        self.assertEqual(largest_tower(people).height, 1)
 
     def test_two_person_tower(self):
         people = [(65, 100), (55, 95)]
-        self.assertEqual(largest_tower(people), 2)
+        self.assertEqual(largest_tower(people).height, 2)
 
     def test_many_people_1(self):
         people = [(100,60), (110, 55), (120, 70), (105, 65), (118, 70), (115, 75), (130, 80) ]
-        self.assertEqual(largest_tower(people), 5)
+        self.assertEqual(largest_tower(people).height, 5)
 
     def test_many_people_2(self):
         people = [
             (65, 100), (70, 150), (56,90), (75, 190), (60, 95), (68, 110)
                 ]
-        self.assertEqual(largest_tower(people), 6)
+        self.assertEqual(largest_tower(people).height, 6)
+
+    def test_many_people_3(self):
+        people = [(50, 100), (60, 110), (70, 120), (80, 130)] 
+        self.assertEqual(largest_tower(people).height, 4)
+
+    def test_many_people_4(self):
+        people = list(reversed([(50, 100), (60, 110), (70, 120), (80, 130)]))
+        self.assertEqual(largest_tower(people).height, 4)
+
+    def test_many_people_5(self):
+        people = [(50, 100), (60, 90), (70, 80), (80, 70)]
+        self.assertEqual(largest_tower(people).height, 1)
 
 
 if __name__ == "__main__":
