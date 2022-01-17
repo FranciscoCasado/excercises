4991c467ea9d1718da8268f4514f6772cfeae8db
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-17 01:02:16 -0300

    added missing tests

--- cracking/ex_1704/tests/test_missing_number.py
+++ cracking/ex_1704/tests/test_missing_number.py
@@ -1,31 +1,36 @@
 import unittest
+from random import shuffle
 
 from cracking.ex_1704.missing_number import missing_number, discard_numbers_with_bit
 
 
 class TestMissingNumber(unittest.TestCase):
-    def test_two_numbers_0(self):
+    def test_two_numbers_miss_0(self):
         l = [1]
         self.assertEqual(missing_number(l), 0)
     
-    def test_two_numbers_1(self):
+    def test_two_numbers_miss_1(self):
         l = [0]
         self.assertEqual(missing_number(l), 1)
     
-    def test_five_numbers_0(self):
+    def test_five_numbers_miss_0(self):
         l = [1, 2, 3, 4]
         self.assertEqual(missing_number(l), 0)
     
-    def test_five_numbers_2(self):
+    def test_five_numbers_miss_2(self):
         l = [0, 1, 3, 4]
         self.assertEqual(missing_number(l), 2)
 
-    def test_ten_numbers_5(self):
+    def test_ten_numbers_miss_5(self):
         l = [0, 1, 2, 3, 4, 6, 7, 8, 9]
         self.assertEqual(missing_number(l), 5)
 
-    def test_ten_numbers_6(self):
+    def test_ten_numbers_miss_6(self):
         l = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]
+        self.assertEqual(missing_number(l), 6)
+    
+    def test_ten_numbers_miss_6_unsorted(self):
+        l = [7, 3, 2, 9, 4, 8, 5, 0, 10, 1]
         self.assertEqual(missing_number(l), 6)
 
 class TestDiscardNumbers(unittest.TestCase):
@@ -43,6 +48,11 @@ class TestDiscardNumbers(unittest.TestCase):
         l = list(range(10))
         discard_numbers_with_bit(l, 1, 1)
         self.assertEqual(l, [0, 1, 4, 5, 8, 9])
+    
+    def test_discard_b1_1(self):
+        l = list(range(10))
+        discard_numbers_with_bit(l, 1, 0)
+        self.assertEqual(l, [2, 3, 6, 7])
 
 if __name__ == "__main__":
     unittest.main()
