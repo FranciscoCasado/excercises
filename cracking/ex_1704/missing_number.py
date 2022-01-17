39dd85303886e4630f42aae9c86427a045e30193
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-17 01:35:20 -0300

    removed prints and formatted with black

--- cracking/ex_1704/missing_number.py
+++ cracking/ex_1704/missing_number.py
@@ -6,27 +6,28 @@ def missing_number(number_list) -> int:
         zeros = 0
         ones = 0
         for i in number_list:
-            zeros = zeros + 1 if  ((i >> bit_index) & 1) == 0 else zeros
-            ones = ones + 1 if  ((i >> bit_index) & 1) == 1 else ones
-            print(f"{bit_index}: {expected} | {zeros} - {ones} -> {missing_number}")
-        if (zeros - ones >=1 ):
+            zeros = zeros + ((i >> bit_index) & 1 == 0)
+            ones = ones + ((i >> bit_index) & 1 == 1)
+
+        if zeros - ones >= 1:
             # bit of missing number is 1
-            missing_number |= (1 << bit_index)
+            missing_number |= 1 << bit_index
             expected -= zeros
             discard_numbers_with_bit(number_list, bit_index, 0)
 
-        if (zeros - ones == 0):
+        if zeros - ones == 0:
             # bit of missing number is 0
             expected -= ones
             discard_numbers_with_bit(number_list, bit_index, 1)
-            
-        if expected == 1: break
-    print(missing_number)
+
+        if expected == 1:
+            break
+
     return missing_number
 
 
 def discard_numbers_with_bit(array, index, value):
     copy = array.copy()
     for i in copy:
-        if ( (i >> index) & 1) == value:
+        if ((i >> index) & 1) == value:
             array.remove(i)
--- cracking/ex_1704/tests/test_missing_number.py
+++ cracking/ex_1704/tests/test_missing_number.py
@@ -8,15 +8,15 @@ class TestMissingNumber(unittest.TestCase):
     def test_two_numbers_miss_0(self):
         l = [1]
         self.assertEqual(missing_number(l), 0)
-    
+
     def test_two_numbers_miss_1(self):
         l = [0]
         self.assertEqual(missing_number(l), 1)
-    
+
     def test_five_numbers_miss_0(self):
         l = [1, 2, 3, 4]
         self.assertEqual(missing_number(l), 0)
-    
+
     def test_five_numbers_miss_2(self):
         l = [0, 1, 3, 4]
         self.assertEqual(missing_number(l), 2)
@@ -28,10 +28,11 @@ class TestMissingNumber(unittest.TestCase):
     def test_ten_numbers_miss_6(self):
         l = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]
         self.assertEqual(missing_number(l), 6)
-    
+
     def test_ten_numbers_miss_6_unsorted(self):
         l = [7, 3, 2, 9, 4, 8, 5, 0, 10, 1]
         self.assertEqual(missing_number(l), 6)
+
 
 class TestDiscardNumbers(unittest.TestCase):
     def test_discard_lsb_1(self):
@@ -43,16 +44,17 @@ class TestDiscardNumbers(unittest.TestCase):
         l = list(range(10))
         discard_numbers_with_bit(l, 0, 0)
         self.assertEqual(l, [1, 3, 5, 7, 9])
-    
+
     def test_discard_b1_1(self):
         l = list(range(10))
         discard_numbers_with_bit(l, 1, 1)
         self.assertEqual(l, [0, 1, 4, 5, 8, 9])
-    
+
     def test_discard_b1_1(self):
         l = list(range(10))
         discard_numbers_with_bit(l, 1, 0)
         self.assertEqual(l, [2, 3, 6, 7])
+
 
 if __name__ == "__main__":
     unittest.main()
