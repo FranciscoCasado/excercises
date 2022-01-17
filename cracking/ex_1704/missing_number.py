1fda21036ca3825e2f7dfc4eb3f0fb90f8a25589
Francisco Casado <fcasado@ug.uchile.cl>
2022-01-17 00:00:12 -0300

    fixed issue in discard_numbers (array copy) and solved if statements

--- cracking/ex_1704/missing_number.py
+++ cracking/ex_1704/missing_number.py
@@ -1,6 +1,6 @@
 def missing_number(number_list) -> int:
 
-    expected = len(number_list)+1
+    expected = len(number_list)
     missing_number = 0
     for bit_index in range(32):
         zeros = 0
@@ -8,8 +8,8 @@ def missing_number(number_list) -> int:
         for i in number_list:
             zeros = zeros + 1 if  ((i >> bit_index) & 1) == 0 else zeros
             ones = ones + 1 if  ((i >> bit_index) & 1) == 1 else ones
-        print(f"{bit_index}: {expected} | {zeros} - {ones} -> {missing_number}")
-        if (zeros - ones > 0):
+            print(f"{bit_index}: {expected} | {zeros} - {ones} -> {missing_number}")
+        if (zeros - ones >=1 ):
             # bit of missing number is 1
             missing_number |= (1 << bit_index)
             expected -= zeros
@@ -21,11 +21,12 @@ def missing_number(number_list) -> int:
             discard_numbers_with_bit(number_list, bit_index, 1)
             
         if expected == 1: break
-
+    print(missing_number)
     return missing_number
 
 
 def discard_numbers_with_bit(array, index, value):
-    for i in array:
+    copy = array.copy()
+    for i in copy:
         if ( (i >> index) & 1) == value:
             array.remove(i)
--- cracking/ex_1704/tests/test_missing_number.py
+++ cracking/ex_1704/tests/test_missing_number.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cracking.ex_1704.missing_number import missing_number
+from cracking.ex_1704.missing_number import missing_number, discard_numbers_with_bit
 
 
 class TestMissingNumber(unittest.TestCase):
@@ -27,6 +27,22 @@ class TestMissingNumber(unittest.TestCase):
     def test_ten_numbers_6(self):
         l = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]
         self.assertEqual(missing_number(l), 6)
+
+class TestDiscardNumbers(unittest.TestCase):
+    def test_discard_lsb_1(self):
+        l = list(range(10))
+        discard_numbers_with_bit(l, 0, 1)
+        self.assertEqual(l, [0, 2, 4, 6, 8])
+
+    def test_discard_lsb_0(self):
+        l = list(range(10))
+        discard_numbers_with_bit(l, 0, 0)
+        self.assertEqual(l, [1, 3, 5, 7, 9])
+    
+    def test_discard_b1_1(self):
+        l = list(range(10))
+        discard_numbers_with_bit(l, 1, 1)
+        self.assertEqual(l, [0, 1, 4, 5, 8, 9])
 
 if __name__ == "__main__":
     unittest.main()
